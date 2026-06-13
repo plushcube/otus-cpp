da3668d00023c832e17fe54c0a10a4e7d7dd87f4
Andrei Chevozerov <plush.cube@mail.ru>
2026-06-13 19:48:44 +0300

    Основная часть задания №4

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,13 @@
-#include <iostream>
 #include <lib.h>
 
-using namespace std;
-
-string format_ip4(const ip4_t &ip4) {
-  return to_string(get<0>(ip4)) + "." +
-         to_string(get<1>(ip4)) + "." +
-         to_string(get<2>(ip4)) + "." +
-         to_string(get<3>(ip4));
-}
-
-auto read_input() {
-  vector<ip4_t> result;
-  for (string line; getline(cin, line), !line.empty();) {
-    result.push_back(read_ip(line));
-  }
-  return result;
-}
-
-void print_pool(const vector<ip4_t> &ip_pool) {
-  for (const auto &ip4 : ip_pool) {
-    cout << format_ip4(ip4) << endl;
-  }
-}
-
 int main(int, char **) {
-  auto ip_pool = read_input();
-
-  sort_rev(ip_pool);
-  print_pool(ip_pool);
-
-  print_pool(filter(ip_pool, [](auto ip4) {
-    return get<0>(ip4) == 1;
-  }));
-  print_pool(filter(ip_pool, [](auto ip4) {
-    return get<0>(ip4) == 46 && get<1>(ip4) == 70;
-  }));
-  print_pool(filter(ip_pool, [](auto ip4) {
-    return get<0>(ip4) == 46 || get<1>(ip4) == 46 || get<2>(ip4) == 46 || get<3>(ip4) == 46;
-  }));
-
+  print_ip(int8_t{-1});
+  print_ip(int16_t{0});
+  print_ip(int32_t{2130706433});
+  print_ip(int64_t{8875824491850138409});
+  print_ip(std::string{"Hello, World!"});
+  print_ip(std::vector<int>{100, 200, 300, 400});
+  print_ip(std::list<int>{400, 300, 200, 100});
+  print_ip(std::make_tuple(123, 456, 789, 0));
   return 0;
 }
