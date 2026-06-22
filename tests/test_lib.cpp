7739b03326cb99e44fa175b861df502aeacdb788
Andrei Chevozerov <184494+plushcube@users.noreply.github.com>
2026-06-22 20:15:53 +0300

    Tasks/task 04 (#4)
    * Основная часть задания №4
    
    * Генерация документации
    
    * Потеряшка
    
    * Документация
    
    * rebase

---
+++ tests/test_lib.cpp
@@ -0,0 +1,80 @@
+#include <gtest/gtest.h>
+#include <lib.h>
+#include <list>
+#include <sstream>
+#include <tuple>
+#include <vector>
+
+/// Test printing of IP defined by a single char.
+///
+TEST(lib_test, test_byte) {
+  std::ostringstream result;
+  print_ip(int8_t{-1}, result);
+  ASSERT_EQ(result.str(), "255\n");
+}
+
+/// Test printing of IP defined by a word (2 chars).
+///
+TEST(lib_test, test_word) {
+  std::ostringstream result;
+  print_ip(int16_t{0}, result);
+  ASSERT_EQ(result.str(), "0.0\n");
+}
+
+/// Test printing of IP defined by a double word (4 chars).
+///
+TEST(lib_test, test_dword) {
+  std::ostringstream result;
+  print_ip(int32_t{2130706433}, result);
+  ASSERT_EQ(result.str(), "127.0.0.1\n");
+}
+
+/// Test printing of IP defined by a quad word (8 chars).
+///
+TEST(lib_test, test_qword) {
+  std::ostringstream result;
+  print_ip(int64_t{8875824491850138409}, result);
+  ASSERT_EQ(result.str(), "123.45.67.89.101.112.131.41\n");
+}
+
+/// Test printing of IP defined by a string.
+///
+TEST(lib_test, test_string) {
+  std::ostringstream result;
+  print_ip(std::string{"Hello, World!"}, result);
+  ASSERT_EQ(result.str(), "Hello, World!\n");
+}
+
+/// Test printing of IP defined by a vector.
+///
+TEST(lib_test, test_vector) {
+  std::ostringstream result;
+  print_ip(std::vector<int>{100, 200, 300, 400}, result);
+  ASSERT_EQ(result.str(), "100.200.300.400\n");
+}
+
+/// Test printing of IP defined by a list.
+///
+TEST(lib_test, test_list) {
+  std::ostringstream result;
+  print_ip(std::list<int>{400, 300, 200, 100}, result);
+  ASSERT_EQ(result.str(), "400.300.200.100\n");
+}
+
+/// Test printing of IP defined by a tuple.
+///
+TEST(lib_test, test_tuple) {
+  std::ostringstream result;
+  print_ip(std::make_tuple(123, 456, 789, 0), result);
+  ASSERT_EQ(result.str(), "123.456.789.0\n");
+}
+
+/// Test printing of IP defined by a tuple with arguments of different types.
+/// Different types should produce a compilation error, so this test should be commented out to allow project to be
+/// compiled. If you want to check if the requirement of the same types still works, just uncomment this test and try to
+/// build the project.
+// TEST(lib_test, test_wrong_tuple) {
+//   std::ostringstream result;
+//   print_ip(std::make_tuple(123, 456, "foo", 0.0), result);
+//   ASSERT_EQ(result.str(), "123.456.foo.0\n");
+// }
