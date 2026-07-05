dc9030c68ea809caa90cd9abe78c9811d0e9016d
Andrei Chevozerov <plush.cube@mail.ru>
2026-07-05 18:29:53 +0300

    Задание №6

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,34 @@
 #include <lib.h>
 
+#include <cassert>
+#include <iostream>
+
 int main(int, char **) {
-  print_ip(int8_t{-1});
-  print_ip(int16_t{0});
-  print_ip(int32_t{2130706433});
-  print_ip(int64_t{8875824491850138409});
-  print_ip(std::string{"Hello, World!"});
-  print_ip(std::vector<int>{100, 200, 300, 400});
-  print_ip(std::list<int>{400, 300, 200, 100});
-  print_ip(std::make_tuple(123, 456, 789, 0));
+  Matrix<int, 0> matrix;
+
+  for (int i = 0; i < 10; ++i) {
+    matrix[i][i] = i;
+    matrix[i][9 - i] = 9 - i;
+  }
+
+  for (int i = 1; i < 9; ++i) {
+    for (int j = 1; j < 9; ++j) {
+      std::cout << matrix[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+
+  std::cout << matrix.size() << std::endl;
+
+  for (const auto &v : matrix) {
+    for (const auto &i : v.first) {
+      std::cout << "[" << i << "]";
+    }
+    std::cout << " " << v.second << std::endl;
+  }
+
+  ((matrix[100][100] = 314) = 0) = 217;
+  // std::cout << matrix[100][100] << std::endl;
+
   return 0;
 }
